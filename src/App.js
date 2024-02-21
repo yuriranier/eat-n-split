ec5e189742ba1593c5598ff986e07bd202e42a66
yuriranier <yuriranier@gmail.com>
2024-02-21 08:43:22 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Button } from './components/Button';
+import { FriendsList } from './components/FriendsList';
+import { FormAddFriend } from './components/FormAddFriend';
+import { FromSplitBill } from './components/FromSplitBill';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialFriends = [
+	{
+		id: 118836,
+		name: 'Clark',
+		image: 'https://i.pravatar.cc/48?u=118836',
+		balance: 0,
+	},
+	{
+		id: 933372,
+		name: 'Sarah',
+		image: 'https://i.pravatar.cc/48?u=933372',
+		balance: 0,
+	},
+	{
+		id: 499476,
+		name: 'Anthony',
+		image: 'https://i.pravatar.cc/48?u=499476',
+		balance: 0,
+	},
+];
+
+export default function App() {
+	const [friends, setFriends] = useState(initialFriends);
+	const [showAddFriend, setShowAddFriend] = useState(false);
+	const [selectedFriend, setSelectedFriend] = useState(null);
+
+	function handleShowAddFriend() {
+		setShowAddFriend((show) => !show);
+	}
+
+	function handleAddFriend(friend) {
+		setFriends((friends) => [...friends, friend]);
+		setShowAddFriend(false);
+	}
+
+	function handleSelection(friend) {
+		setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+		setShowAddFriend(false);
+	}
+
+	function handleSplitBill(value) {
+		console.log(value);
+		setFriends((friends) =>
+			friends.map((friend) =>
+				friend.id === selectedFriend.id
+					? { ...friend, balance: friend.balance + value }
+					: friend
+			)
+		);
+		setSelectedFriend(null);
+	}
+
+	return (
+		<div className='app'>
+			<div className='sidebar'>
+				<FriendsList
+					friends={friends}
+					selectedFriend={selectedFriend}
+					onSelection={handleSelection}
+				/>
+
+				{showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
+				<Button onClick={handleShowAddFriend}>
+					{showAddFriend ? 'Close' : 'Add Friend'}
+				</Button>
+			</div>
+			{selectedFriend && (
+				<FromSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />
+			)}
+		</div>
+	);
 }
-
-export default App;
